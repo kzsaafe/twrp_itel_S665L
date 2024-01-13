f4d219bd3443a8f7809beb6c69b225f942c6a898
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-01-13 20:47:10 +0700

    Update twrp_S665L.mk

--- twrp_S665L.mk
+++ twrp_S665L.mk
@@ -7,13 +7,18 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from itel-S665L device
 $(call inherit-product, device/itel/S665L/device.mk)
+
+
+
 
 PRODUCT_DEVICE := S665L
 PRODUCT_NAME := twrp_iS665L
