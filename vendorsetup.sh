15fb4b261c28a8b9652988447e2337b2e27f3afc
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-01-14 08:32:39 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_itel-S665L-user
-add_lunch_combo omni_itel-S665L-userdebug
-add_lunch_combo omni_itel-S665L-eng
+add_lunch_combo twrp_S665L-user
+add_lunch_combo twrp_S665L-userdebug
+add_lunch_combo twrp_S665L-eng
