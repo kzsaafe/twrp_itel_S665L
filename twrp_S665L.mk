2227c40109733bab00b88659b41ec4a2de68abe7
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-01-14 08:15:53 +0700

    Update twrp_S665L.mk

--- twrp_S665L.mk
+++ twrp_S665L.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
@@ -25,8 +25,11 @@ PRODUCT_NAME := twrp_S665L
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel S665L
 PRODUCT_MANUFACTURER := itel
+PRODUCT_RELEASE_NAME := Itel Itel S665L
 
-PRODUCT_GMS_CLIENTID_BASE := android-transsion
+
+
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="S665L-user 12 SP1A.210812.016 490 release-keys"
